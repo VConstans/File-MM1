de98d13e5cbb9dbc39a3f72d6012c6f20a6dd88f
Victor <victor.constans@etu.unistra.fr>
2017-10-05 23:29:33 +0200

    Print statistic

--- MM1.java
+++ MM1.java
@@ -2,11 +2,14 @@ public class MM1
 {
 	public static void main(String[] args)
 	{
-		Ech echeancier = new Ech(5,6,Integer.parseInt(args[0]));
+		Stats statSimu = new Stats(5,6,Integer.parseInt(args[0]));
+		Ech echeancier = new Ech(5,6,Integer.parseInt(args[0]),statSimu);
 
 		while(!echeancier.finEch())
 		{
 			echeancier.traitementEvt();
 		}
+		statSimu.resTheorique();
+		statSimu.resSimulation();
 	}
 }
