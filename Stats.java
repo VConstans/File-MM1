e1af95d0bbc28f8cef5b34df7abde4b7110f3c4b
Victor <victor.constans@etu.unistra.fr>
2017-10-05 23:28:19 +0200

    Add statistic functions

---
+++ Stats.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+public class Stats
+{
+	//Attribut partie theorqiue
+	private double lambda;
+	private double mu;
+	private double ro;
+	private double duree;
+
+	//Attributs partie simulation
+	private double nbClientTot=0;
+	private double clientSansAttente=0;
+	private double clientAvecAttente=0;
+	private ArrayList<Double> tmpSejourClient = new ArrayList<Double>();
+	private ArrayList<Double> nbClientSysteme = new ArrayList<Double>();
+	private double derniereDate = 0;
+	
+
+	public Stats(int lambda, int mu, double duree)
+	{
+		this.lambda = lambda;
+		this.mu = mu;
+		this.duree = duree;
+//		nbClientSysteme.add(0,new Double(0));
+	}
+
+	public void resTheorique()
+	{
+		System.out.println("-------------\nRESULTAT THEORIQUE\n-----------");
+
+		if(lambda < mu)
+		{
+			ro = lambda/mu;
+
+			System.out.println("lambda < mu : file stable");
+			System.out.println("ro = "+ro);
+			System.out.println("Nombre de clients attendus (lambda*duree) = "+(lambda*duree));
+			System.out.println("Probabilite de service sans attente (1-ro) = "+(1-ro));
+			System.out.println("Probabilite de file occupee (ro) = "+ro);
+			System.out.println("Debit (lambda) = "+lambda);
+			System.out.println("Esperance nombre de clients (ro/(1-ro))"+(ro/(1-ro)));
+			System.out.println("Temps moyen de sejour (1/(mu*(1-ro)))"+(1/(mu*(1-ro))));
+		}
+		else
+		{
+			System.out.println("lambda > mu : file non stable");
+		}
+	}
+	
+	public void resSimulation()
+	{
+
+		double nbMoyenClientSysteme = 0;
+		int j=0;
+		for(j=0;j<nbClientSysteme.size();j++)
+		{
+			nbMoyenClientSysteme += j*(nbClientSysteme.get(j).doubleValue());
+		}
+
+		double tmpMoyenSejour = 0;
+		int i = 0;
+		for(i=0;i<tmpSejourClient.size();i++)
+		{
+			tmpMoyenSejour += tmpSejourClient.get(i).doubleValue();
+		}
+		
+		System.out.println("\n\n\n-------------\nRESULTAT SIMULATION\n-----------");
+		System.out.println("Nombre total de clients = "+nbClientTot);
+		System.out.println("Proportion de client sans attente = "+(clientSansAttente/nbClientTot));
+		System.out.println("Proportion de client avec attente = "+(clientAvecAttente/nbClientTot));
+		System.out.println("Debit = "+(nbClientTot/duree));
+		System.out.println("Nombre moyen de client dans le systeme = "+(nbMoyenClientSysteme/duree));
+		System.out.println("Temps moyen de sejour = "+(tmpMoyenSejour/nbClientTot));
+	}
+
+
+	public void addNbClientSysteme(int nb, double date)
+	{
+		double dureeCalc = date - derniereDate;
+
+		if(nb < nbClientSysteme.size())
+		{
+			nbClientSysteme.set(nb,new Double(nbClientSysteme.get(nb).doubleValue()+dureeCalc));
+		}
+		else
+		{
+			nbClientSysteme.add(nb,new Double(dureeCalc));
+		}
+
+		derniereDate = date;
+	}
+
+	public void addSejourClient(double dateCourant,double nouvDate,int client)
+	{
+		tmpSejourClient.add(client,new Double(nouvDate-dateCourant));
+	}
+
+	public void ajoutClient()
+	{
+		nbClientTot +=1;
+	}
+
+	public void ajoutClientSansAttente()
+	{
+		System.out.println("Sans attente");
+		clientSansAttente += 1;
+	}
+
+	public void ajoutClientAvecAttente()
+	{
+		System.out.println("Avec attente");
+		clientAvecAttente += 1;
+	}
+
+}
+
